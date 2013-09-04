a882652d8f308deb362776d1ed229f0203b5934c
Axel Haustant <noirbizarre@gmail.com>
2013-09-04 16:41:10 +0200

    Better publish button and search where field behavior

--- weckan/static/js/site.js
+++ weckan/static/js/site.js
@@ -5,6 +5,13 @@
 
     "use strict";
 
+    var hide_where = function() {
+        if (!$('#where-group input').val() && !$('#where-group input').is(':focus')) {
+            $('#where-group').collapse('hide');
+        }
+    }
+
+
     $(function() {
         // Fix collapse on sidebar
         $("#sidebar .panel-heading a").on('click', function (e) { e.preventDefault(); });
@@ -12,6 +19,15 @@
         // Display tooltips
         $('[rel=tooltip]').tooltip();
         $('[rel=popover]').popover();
+
+        // Search field behavior
+        $('#search-input').on('focus', function() {
+            $('#where-group').collapse('show');
+        }).on('blur', function() {
+            setTimeout(hide_where, 500);
+        });
+
+        $('#where-group input').on('blur', hide_where);
     });
 
 
