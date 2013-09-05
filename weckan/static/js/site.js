c3170031d0d782b97f6d60451cac20e0ee34b6c0
Axel Haustant <noirbizarre@gmail.com>
2013-09-05 12:15:07 +0200

    Handle search-where field behavior

--- weckan/static/js/site.js
+++ weckan/static/js/site.js
@@ -5,29 +5,121 @@
 
     "use strict";
 
+    var TERRYTORIES_END_POINT = 'http://ou.comarquage.fr/api/v1/autocomplete-territory',
+        TERRITORIES_KIND = [
+            'ArrondissementOfCommuneOfFrance',
+            'CommuneOfFrance',
+            'Country',
+            'DepartmentOfFrance',
+            'OverseasCollectivityOfFrance',
+            'RegionOfFrance'
+        ],
+        API_URL = TERRYTORIES_END_POINT + '?' + $.param({ kind: TERRITORIES_KIND, term: '_QUERY'}, true),
+        COOKIE_NAME = 'territory-infos';
+
+    /**
+     * Hide the "where" field
+     */
     var hide_where = function() {
-        if (!$('#where-group input').val() && !$('#where-group input').is(':focus')) {
+        if (!$('#ext_territory').val() && !$('#where-group input').is(':focus')) {
             $('#where-group').collapse('hide');
         }
+    };
+
+    /**
+     * Filter the Territory API to match Typeahead expected format.
+     */
+    var filter_territory_api = function(response) {
+        return response.data.items;
+    };
+
+    /**
+     * Fix JSONP compatibility between API and typeahead.js
+     */
+    var fix_url = function(jqXhr, settings) {
+        settings.url = settings.url.replace('callback', 'jsonp');
+    };
+
+    /**
+     * Set the territory to filter
+     * @param {string} value   the territory key (kind/code)
+     * @param {string} display the territory display name
+     */
+    var set_territory = function(value, display) {
+        $('#ext_territory').val(value);
+        if (display) {
+            $('#where-input').val(display);
+        } else {
+            display = $('#where-input').val();
+        }
+
+        $('#where-group .glyphicon')
+            .removeClass('glyphicon-globe')
+            .addClass('glyphicon-remove')
+            .addClass('territory-clear');
+        $('#where-group').collapse('show');
+
+        // Persist the filter into the cookie
+        $.cookie(COOKIE_NAME, value + '|' + display, {path: '/'});
+    };
+
+    /**
+     * Clear the current territory filter
+     */
+    var clear_territory = function() {
+        $.removeCookie(COOKIE_NAME, {path: '/'});
+        $('#where-input').val(null);
+        $('#ext_territory').val(null);
+        $('#where-group .glyphicon')
+            .removeClass('glyphicon-remove')
+            .removeClass('territory-clear')
+            .addClass('glyphicon-globe');
     }
 
 
     $(function() {
+        // Force cookie raw
+        $.cookie.raw = true;
+
         // Fix collapse on sidebar
         $("#sidebar .panel-heading a").on('click', function (e) { e.preventDefault(); });
 
-        // Display tooltips
+        // Display tooltips and popovers
         $('[rel=tooltip]').tooltip();
         $('[rel=popover]').popover();
 
         // Search field behavior
-        $('#search-input').on('focus', function() {
-            $('#where-group').collapse('show');
-        }).on('blur', function() {
-            setTimeout(hide_where, 500);
-        });
+        $('#search-input')
+            .on('focus', function() {
+                $('#where-group').collapse('show');
+            })
+            .on('blur', function() {
+                setTimeout(hide_where, 500);
+            });
 
-        $('#where-group input').on('blur', hide_where);
+        $('#where-input').on('blur', hide_where)
+            .typeahead({
+                name: 'territories',
+                valueKey: 'main_postal_distribution',
+                remote: {
+                    url: API_URL,
+                    wildcard: '_QUERY',
+                    dataType: 'jsonp',
+                    filter: filter_territory_api,
+                    beforeSend: fix_url
+                }
+            })
+            .on('typeahead:selected typeahead:autocompleted', function(e, territory) {
+                set_territory(territory.kind + '/' + territory.code);
+                $('#search-input').focus();
+            });
+
+        $('#where-group').on('click', '.territory-clear', clear_territory);
+
+        // Restore territory state from cookie
+        if ($.cookie(COOKIE_NAME)) {
+            set_territory.apply(null, $.cookie(COOKIE_NAME).split('|'));
+        }
     });
 
 
