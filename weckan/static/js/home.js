f223933ae595cbd2c842f4fa4b0e7b198b37ff1d
Axel Haustant <noirbizarre@gmail.com>
2013-10-30 01:37:42 +0100

    Fixes

--- weckan/static/js/home.js
+++ weckan/static/js/home.js
@@ -5,24 +5,24 @@
 
     "use strict";
 
-    var MIN_WIDTH_FOR_EQUAL_HEIGHT = 767;
+    // var MIN_WIDTH_FOR_EQUAL_HEIGHT = 767;
 
-    /**
-     * Equal for featured panels
-     */
-    var handle_panel_height = function() {
-        if ($(window).width() >= MIN_WIDTH_FOR_EQUAL_HEIGHT) {
-            $('.marketing .panel').equalHeights();
-        } else {
-            $('.marketing .panel').css({ height: 'auto' });
-        }
-    };
+    // /**
+    //  * Equal for featured panels
+    //  */
+    // var handle_panel_height = function() {
+    //     if ($(window).width() >= MIN_WIDTH_FOR_EQUAL_HEIGHT) {
+    //         $('.marketing .panel').equalHeights();
+    //     } else {
+    //         $('.marketing .panel').css({ height: 'auto' });
+    //     }
+    // };
 
 
-    $(function() {
-        // Equal height for marketing panels
-        $(window).resize(handle_panel_height);
-        handle_panel_height();
-    });
+    // $(function() {
+    //     // Equal height for marketing panels
+    //     $(window).resize(handle_panel_height);
+    //     handle_panel_height();
+    // });
 
 }(window.jQuery));
