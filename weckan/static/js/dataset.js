facc2117f0ad0f648a17c2af799a3f4a8f0ebcd2
Axel Haustant <noirbizarre@gmail.com>
2013-11-14 13:58:00 +0100

    Use YouCKAN toggle_featured

--- weckan/static/js/dataset.js
+++ weckan/static/js/dataset.js
@@ -5,4 +5,44 @@
 
     "use strict";
 
+    var message_template = swig.compile($('#message-template').html()),
+        community_container = '.community_container > div.container';
+
+    var display_message = function(message, type, container) {
+        container = container || '.dataset-container';
+        $(container).prepend(message_template({
+            level: type == 'error' ? 'danger' : type,
+            message: message
+        }));
+    };
+
+    var translate = function(key) {
+        return $('meta[name="'+key+'-translation"]').attr('content');
+    };
+
+    $(function() {
+        // Async feature handling
+        $('a.featured').click(function() {
+            var $this = $(this),
+                api_url = $this.data('api');
+
+            $.get(api_url, function(data) {
+                if (data.featured) {
+                    $this.removeClass('btn-default').addClass('btn-success');
+                    $this.attr('title', $this.data('featured-title'));
+                    display_message(translate('is-featured'), 'success', community_container);
+                } else {
+                    $this.removeClass('btn-success').addClass('btn-default');
+                    $this.attr('title', $this.data('unfeatured-title'));
+                    display_message(translate('is-unfeatured'), 'success', community_container);
+                }
+            }).error(function(e) {
+                console.error(e);
+                display_message(translate('featured-error'), 'error', community_container);
+            });
+
+            return false;
+        });
+    });
+
 }(window.jQuery, window.swig));
--- weckan/static/js/home.js
+++ weckan/static/js/home.js
@@ -1,12 +1,50 @@
 /**
  * Homepage specific features
  */
-(function($){
+(function($, swig){
 
     "use strict";
 
-    $('#home-carousel .title').on('slide.bs.carousel', function() {
-        $(this).trigger('update');
+
+    var message_template = swig.compile($('#message-template').html());
+
+    var display_message = function(message, type, container) {
+        container = container || 'section.default .container';
+        $(container).prepend(message_template({
+            level: type == 'error' ? 'danger' : type,
+            message: message
+        }));
+    };
+
+    var translate = function(key) {
+        return $('meta[name="'+key+'-translation"]').attr('content');
+    };
+
+    $(function() {
+        // Update carousel ellipsis on change
+        $('#home-carousel .title').on('slide.bs.carousel', function() {
+            $(this).trigger('update');
+        });
+
+        // Async feature handling
+        $('a.unfeature').click(function() {
+            var $this = $(this),
+                api_url = $this.data('api');
+
+            if (!$this.hasClass('disabled')) {
+                $.get(api_url, function(data) {
+                    $this.removeClass('btn-warning').addClass('btn-danger').addClass('disabled');
+                    $this.removeAttr('href').attr('title', translate('is-unfeatured'));
+                    display_message(translate('is-unfeatured'), 'success');
+                }).error(function(e) {
+                    console.error(e);
+                    display_message(translate('featured-error'), 'error');
+                });
+            }
+
+
+            return false;
+        });
     });
 
-}(window.jQuery));
+}(window.jQuery, window.swig));
