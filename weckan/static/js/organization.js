859353692c49edf95262e6a9713fd80410f0cc74
Axel Haustant <noirbizarre@gmail.com>
2013-11-20 17:09:37 +0100

    Ajax organization subscription

--- weckan/static/js/organization.js
+++ weckan/static/js/organization.js
@@ -5,11 +5,10 @@
 
     "use strict";
 
-    var message_template = swig.compile($('#message-template').html()),
-        community_container = '.community_container > div.container';
+    var message_template = swig.compile($('#message-template').html());
 
     var display_message = function(message, type, container) {
-        container = container || '.dataset-container';
+        container = container || '.organization-container';
         $(container).prepend(message_template({
             level: type == 'error' ? 'danger' : type,
             message: message
@@ -21,29 +20,6 @@
     };
 
     $(function() {
-        // Async feature handling
-        $('a.featured').click(function() {
-            var $this = $(this),
-                api_url = $this.data('api');
-
-            $.get(api_url, function(data) {
-                if (data.featured) {
-                    $this.removeClass('btn-default').addClass('btn-success');
-                    $this.attr('title', $this.data('featured-title'));
-                    display_message(translate('is-featured'), 'success', community_container);
-                } else {
-                    $this.removeClass('btn-success').addClass('btn-default');
-                    $this.attr('title', $this.data('unfeatured-title'));
-                    display_message(translate('is-unfeatured'), 'success', community_container);
-                }
-            }).error(function(e) {
-                console.error(e);
-                display_message(translate('featured-error'), 'error', community_container);
-            });
-
-            return false;
-        });
-
         // Async follow handling
         $('a.follow').click(function() {
             var $this = $(this),
