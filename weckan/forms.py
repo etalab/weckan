93a64f8adc055a18de82940843e18830480859bb
Axel Haustant <axel@data.gouv.fr>
2014-01-17 18:32:40 +0100

    Fix TerrytoryField casing

--- weckan/forms.py
+++ weckan/forms.py
@@ -259,8 +259,20 @@ class TagField(StringField):
                 raise validators.ValidationError(message % tag)
 
 
-class TerritoryField(TagField):
+class TerritoryField(StringField):
     widget = TerritoryAutocompleter()
+
+    def _value(self):
+        if self.data:
+            return u','.join(self.data)
+        else:
+            return u''
+
+    def process_formdata(self, valuelist):
+        if valuelist:
+            self.data = list(set([x.strip() for x in valuelist[0].split(',') if x.strip()]))
+        else:
+            self.data = []
 
     def pre_validate(self, form):
         pass
